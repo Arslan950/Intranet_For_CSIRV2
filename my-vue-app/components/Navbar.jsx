f135bf0bdddd847f2b56831957265c466d9180ac
Roshan <roshan20031112@gmail.com>
2025-07-16 23:43:28 +0530

    added login/out on mobile screen

--- my-vue-app/components/Navbar.jsx
+++ my-vue-app/components/Navbar.jsx
@@ -79,16 +79,12 @@ const Navbar = (props) => {
           <div className="sm:text-lg text-xs hidden sm:block  ">Center of Excellence and Farm Machinery, Ludhiana</div>
         </Link> 
 
-        {/* <Link
-          to="https://github.com/Light200312/Intranet_For_CSIRV2.git"
-          className="ml-auto mr-4"
-        >
-          <img
-            src="/GHIcon.png"
-            alt="GitHub Icon"
-            className="sm:h-10 hover:invert-0 object-contain hover:bg-white cursor-pointer rounded-full invert transition"
-          />
-        </Link> */}
+           <a onClick={handleLogout} className="hover:underline sm:hidden flex items-center gap-1 cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" height="14px" viewBox="0 -960 960 960" width="24px" fill="#ffffffff">
+              <path d="M480-120q-75 0-140.5-28.5t-114-77q-48.5-48.5-77-114T120-480q0-75 28.5-140.5t77-114q48.5-48.5 114-77T480-840v80q-117 0-198.5 81.5T200-480q0 117 81.5 198.5T480-200v80Zm160-160-56-57 103-103H360v-80h327L584-624l56-56 200 200-200 200Z" />
+            </svg>
+            <p>{props.authenticated ? "Logout" : "Login"}</p>
+          </a>
       </nav>
         </div>
     </div>
